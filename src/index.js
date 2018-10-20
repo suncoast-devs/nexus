c4f61e3809f5bb4f6eef18c6ad5e46ec2c7d0f0f
Jason L Perry <jason@suncoast.io>
2018-10-20 12:22:31 -0400

    Hello, React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import './styles/screen.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
