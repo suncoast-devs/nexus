68db73b5e7170f99b5ca79311a7f9d8c33d458d7
Gavin Stark <gavin@gstark.com>
2019-04-30 15:24:38 -0400

    Profile derives from Person

--- src/components/models/Profile.js
+++ src/components/models/Profile.js
@@ -1,13 +1,10 @@
-import ApplicationRecord from './ApplicationRecord'
-import { attr } from 'spraypaint'
 import Person from './Person'
 
-const Profile = ApplicationRecord.extend({
+// The Profile attributes are the same as the person
+const Profile = Person.extend({
   static: {
     jsonapiType: 'profiles'
-  },
-  // The Profile attributes are the same as the person
-  attrs: Person.attrs
+  }
 })
 
 export default Profile
