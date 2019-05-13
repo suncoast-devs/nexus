8c2723697703325b8147c44e9b7d54f3600f5dda
Gavin Stark <gavin@gstark.com>
2019-05-13 17:03:41 -0400

    Show a welcome message on the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import SlackInviteMessage from './SlackInviteMessage'
 import GithubSetupMessage from './GithubSetupMessage'
+import banner from '../../images/banner.svg'
 
 const Home = ({ profile, isAuthenticated }) => {
   return (
     <>
-      <SlackInviteMessage profile={profile} />
-      <GithubSetupMessage profile={profile} />
-      {!isAuthenticated && (
+      {isAuthenticated ? (
+        <section className="section">
+          <div className="container">
+            <article className="message is-info is-medium">
+              <div className="message-header">
+                <p>Welcome to Nexus</p>
+              </div>
+              <div className="message-body" style={{ textAlign: 'center' }}>
+                <p>
+                  <img src={banner} style={{ width: '25%' }} />
+                </p>
+                <p>Welcome to Nexus, your student information management system</p>
+              </div>
+            </article>
+          </div>
+        </section>
+      ) : (
         <div>
           <h4>You are not logged in!</h4>
         </div>
       )}
+      <GithubSetupMessage profile={profile} />
+      <SlackInviteMessage profile={profile} />
     </>
   )
 }
