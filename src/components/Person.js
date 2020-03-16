92eb4ce23edf0c727eebb55ac5faaf980b552162
Gavin Stark <gavin@gstark.com>
2020-03-16 18:35:09 -0400

    Adds a fallback icon if the person's profile image won't load

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const iconStyle = { width: '32px', height: '32px', textAlign: 'center', verticalAlign: 'center', lineHeight: '32px' }
 
+const PersonImage = ({ alt, url }) => {
+  const [error, setError] = useState(!url)
+
+  if (error) {
+    return <i style={iconStyle} className="fas fa-user" />
+  } else {
+    return <img alt={alt} src={url} onError={() => setError(true)} />
+  }
+}
 const Person = ({ person }) => {
   return (
     <article className="media">
       <figure className="media-left">
-        {person.smallProfileImageUrl ? (
-          <img alt={person.fullName} src={person.smallProfileImageUrl} />
-        ) : (
-          <i style={iconStyle} className="fas fa-user" />
-        )}
+        <PersonImage alt={person.fullName} url={person.smallProfileImageUrl} />
       </figure>
       <div className="media-content">
         <div className="content">{person.fullName}</div>
