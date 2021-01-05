321e7fa94f6c55cbb3c49f35ead680b5b8022d1f
Gavin Stark <gavin@gstark.com>
2021-01-05 15:47:47 -0500

    Updates the text of the slack invite message

--- src/components/home/SlackInviteMessage.js
+++ src/components/home/SlackInviteMessage.js
@@ -7,10 +7,19 @@ const SlackInviteMessage = ({ profile }) =>
         <article className="message is-warning is-medium">
           <div className="message-header">
             <p>You have not connected your Nexus account to Slack</p>
+            <p>
+              By connecting your Slack account you will receive important notices when homework is assigned and graded,
+              and when you receive a progress report.
+            </p>
           </div>
           <div className="message-body">
-            Send the message <code>register {profile.slackInviteCode}</code> to <code>nexus</code> in a direct message
-            to start to receive notices from Nexus on Slack.
+            <p>To connect:</p>
+            <p>
+              Enter <code>nexus</code> in the search bar at the top of the Slack window
+            </p>
+            <p>Select the Nexus (app) option to begin a "direct message" and type the following as your message:</p>
+            <p />
+            <p>register {profile.slackInviteCode}</p>
           </div>
         </article>
       </div>
