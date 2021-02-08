46b5ff5a12e0bb0b8917fd22e5336495d6066048
Gavin Stark <gavin@gstark.com>
2021-02-08 16:14:12 -0500

    Minor refactoring, extracting a local component

--- src/components/gradebook/StudentGradebook.js
+++ src/components/gradebook/StudentGradebook.js
@@ -6,6 +6,67 @@ import { Cohort, Assignment } from '@/components/models'
 import useModelData from '@/hooks/useModelData'
 import LoadingIndicator from '@/components/utils/LoadingIndicator'
 
+const StudentGradebookCohort = ({ cohort, profile }) => {
+  const studentEnrollment = cohort.studentEnrollments.find(
+    enrollment => parseInt(enrollment.personId) === parseInt(profile.id)
+  )
+
+  const homeworksNeededForCompletion = cohort.homeworks.filter(homework => homework.countsTowardsCompletion)
+  const cohortAssignments =
+    (studentEnrollment &&
+      studentEnrollment.person &&
+      studentEnrollment.person.assignments
+        .sort((a, b) => b.id - a.id)
+        .filter(assignment => parseInt(assignment.homework.cohortId) === parseInt(cohort.id))) ||
+    []
+
+  if (cohortAssignments.length === 0) {
+    return <></>
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className="title">{cohort.name}</h1>
+
+      {homeworksNeededForCompletion.length > 16 && (
+        <div className="notification is-primary">
+          You have completed <strong>{studentEnrollment.completedHomeworkCount}</strong> assignments.
+          {studentEnrollment.incompleteHomeworkCount > 0 && (
+            <span>
+              You have <strong>{studentEnrollment.incompleteHomeworkCount}</strong> incomplete assignments
+            </span>
+          )}
+        </div>
+      )}
+
+      <table className="table is-fullwidth is-hoverable">
+        <thead>
+          <tr>
+            <th>Assignment</th>
+            <th>Grade</th>
+            <th>Turned In</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cohortAssignments.map(assignment => {
+            return (
+              <tr key={assignment.id}>
+                <td>
+                  <Link to={`/assignment/${assignment.id}`}>{assignment.homework.title}</Link>
+                </td>
+                <td>{assignment.turnedIn && Assignment.scoreInfo(assignment.score).title}</td>
+                <td className={cx({ 'has-text-danger': !assignment.turnedIn && assignment.overdue })}>
+                  {assignment.turnedIn ? 'Yes' : assignment.overdue ? 'Overdue' : 'No'}
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </React.Fragment>
+  )
+}
+
 const StudentGradebook = ({ profile, showTitle }) => {
   const { loading: loadingCohorts, data: cohorts } = useModelData(() =>
     Cohort.includes(['homeworks', { student_enrollments: { person: { assignments: 'homework' } } }])
@@ -25,64 +86,9 @@ const StudentGradebook = ({ profile, showTitle }) => {
     <section className="section">
       <div className="container">
         {showTitle && <h1 className="title">Grades for: {profile.fullName}</h1>}
-        {cohorts.map(cohort => {
-          const studentEnrollment = cohort.studentEnrollments.find(
-            enrollment => parseInt(enrollment.personId) === parseInt(profile.id)
-          )
-
-          const homeworksNeededForCompletion = cohort.homeworks.filter(homework => homework.countsTowardsCompletion)
-          const cohortAssignments =
-            (studentEnrollment &&
-              studentEnrollment.person &&
-              studentEnrollment.person.assignments
-                .sort((a, b) => b.id - a.id)
-                .filter(assignment => parseInt(assignment.homework.cohortId) === parseInt(cohort.id))) ||
-            []
-
-          return cohortAssignments.length > 0 ? (
-            <React.Fragment key={cohort.id}>
-              <h1 className="title">{cohort.name}</h1>
-
-              {homeworksNeededForCompletion.length > 16 && (
-                <div className="notification is-primary">
-                  You have completed <strong>{studentEnrollment.completedHomeworkCount}</strong> assignments.
-                  {studentEnrollment.incompleteHomeworkCount > 0 && (
-                    <span>
-                      You have <strong>{studentEnrollment.incompleteHomeworkCount}</strong> incomplete assignments
-                    </span>
-                  )}
-                </div>
-              )}
-
-              <table className="table is-fullwidth is-hoverable">
-                <thead>
-                  <tr>
-                    <th>Assignment</th>
-                    <th>Grade</th>
-                    <th>Turned In</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cohortAssignments.map(assignment => {
-                    return (
-                      <tr key={assignment.id}>
-                        <td>
-                          <Link to={`/assignment/${assignment.id}`}>{assignment.homework.title}</Link>
-                        </td>
-                        <td>{assignment.turnedIn && Assignment.scoreInfo(assignment.score).title}</td>
-                        <td className={cx({ 'has-text-danger': !assignment.turnedIn && assignment.overdue })}>
-                          {assignment.turnedIn ? 'Yes' : assignment.overdue ? 'Overdue' : 'No'}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </React.Fragment>
-          ) : (
-            <React.Fragment key={cohort.id} />
-          )
-        })}
+        {cohorts.map(cohort => (
+          <StudentGradebookCohort key={cohort.id} cohort={cohort} profile={profile} />
+        ))}
       </div>
     </section>
   )
