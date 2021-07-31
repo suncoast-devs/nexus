3939840d0b4d8eef62375d128c3ef0142d8bad1a
Gavin Stark <gavin@gstark.com>
2021-07-31 07:09:18 -0400

    Showing addition data point information about an assignment turn-in

--- src/components/assignments/TurnedInAssignment.jsx
+++ src/components/assignments/TurnedInAssignment.jsx
@@ -5,6 +5,10 @@ import { GistTurnedInAssignmentDetails } from './GistTurnedInAssignmentDetails'
 import { URLTurnedInAssignmentDetails } from './URLTurnedInAssignmentDetails'
 
 export function TurnedInAssignment({ homework, assignmentEvent }) {
+  function wordCase(string) {
+    return string ? string.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase()) : null
+  }
+
   return (
     <>
       <div className="notification is-success is-light">
@@ -23,6 +27,32 @@ export function TurnedInAssignment({ homework, assignmentEvent }) {
           </div>
         </div>
       </div>
+      {assignmentEvent.payload.level ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Level</th>
+              <th>Difficulty</th>
+              <th>Lecture Prepared</th>
+              <th>Total Hours</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{wordCase(assignmentEvent.payload.level)}</td>
+            </tr>
+            <tr>
+              <td>{wordCase(assignmentEvent.payload.difficulty)}</td>
+            </tr>
+            <tr>
+              <td>{wordCase(assignmentEvent.payload.lecturePreparedMe)}</td>
+            </tr>
+            <tr>
+              <td>{wordCase(assignmentEvent.payload.totalHours)}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : null}
       {assignmentEvent.payload.comment && <MarkDownDiv markdown={assignmentEvent.payload.comment} />}
     </>
   )
