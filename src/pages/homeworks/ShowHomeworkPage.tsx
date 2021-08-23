82451166a8d2491ff67001f02a1c44858d63269c
Gavin Stark <gavin@gstark.com>
2021-08-23 14:01:45 -0400

    Dynamically replace $GITHUB_USER with the user github account

--- src/pages/homeworks/ShowHomeworkPage.tsx
+++ src/pages/homeworks/ShowHomeworkPage.tsx
@@ -3,23 +3,27 @@ import { Homework, UnProxyRecord } from '@/components/models'
 import { MarkDownDiv } from '@/components/utils/MarkDownDiv'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router'
+import useProfile from '@/hooks/useProfile'
 
 export function ShowHomeworkPage() {
+  const { isLoading: isLoadingProfile, profile } = useProfile()
   const { cohortId, homeworkId } = useParams<{ cohortId: string; homeworkId: string }>()
   const { isLoading, data: homework = new Homework() } = useQuery(['homework', cohortId, homeworkId], () =>
     Homework.find(homeworkId).then(UnProxyRecord)
   )
 
-  if (isLoading) {
+  if (isLoading || isLoadingProfile) {
     return <></>
   }
+
+  const homeworkWithGithubUserReplaced = homework.bodyWithResolvedUrls.replaceAll('$GITHUB_USER', profile.github)
 
   return (
     <section className="section">
       <div className="container">
         <div className="content mb-4">
           <h1>{homework.title}</h1>
-          <MarkDownDiv markdown={homework.bodyWithResolvedUrls} />
+          <MarkDownDiv markdown={homeworkWithGithubUserReplaced} />
         </div>
       </div>
     </section>
